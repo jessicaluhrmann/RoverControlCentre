dc197d536e1f2f15e4ec5139996adba17ed27d40
jessicaluhrmann <jessica.luhrmann@xplortechnologies.com>
2023-10-30 10:26:01 +1000

    Add project files.

---
+++ RoverControlCentre/IDirectable.cs
@@ -0,0 +1,9 @@
+﻿namespace RoverControlCentre
+{
+    interface IDirectable
+    {
+        string GetInfo();
+        string Explore();
+        string Collect();
+    }
+}
---
+++ RoverControlCentre/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace RoverControlCentre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MoonRover lunokhod = new MoonRover("Lunokhod 1", 1970);
+            MoonRover apollo = new MoonRover("Apollo 15", 1971);
+            MarsRover sojourner = new MarsRover("Sojourner", 1997);
+            Satellite sputnik = new Satellite("Sputnik", 1957);
+
+            IDirectable[] probes = new IDirectable[] {lunokhod, apollo, sojourner, sputnik};
+
+            DirectAll(probes);
+        }
+
+        public static void DirectAll(IDirectable[] probes)
+        {
+            foreach (var probe in probes)
+            {
+                Console.WriteLine(probe.GetInfo());
+                Console.WriteLine(probe.Explore());
+                Console.WriteLine(probe.Collect());
+            }
+        }
+    }
+}
